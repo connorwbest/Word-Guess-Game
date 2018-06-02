2d94d60e47cd645ae050accad2bf01610f6326aa
connorw <connorwbest95@gmail.com>
2018-06-01 20:55:30 -0400

    Completed first loop, broken input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,36 @@
 //Variables 
 var words = ["alaska","alabama","arkansas","arizona","california","colorado","connecticut","delaware","florida","georgia","hawaii","iowa","idaho","illinois","indiana","kansas","kentucky","louisiana","massachusetts","maryland","maine","michigan","minnesota","missouri","mississippi","montana","north carolina","north dakota","nebraska","new hampshire","new jersey","new mexico","nevada","new york","ohio","oklahoma","oregon","pennsylvania","puerto rico","rhode island","south carolina","south dakota","tennessee","texas","utah","virginia","vermont","washington","wisconsin","west virginia","wyoming"];
 var arrayGuess = []; 
-var arrayWord = words[Math.floor(Math.random() * words.length)].split("");
-var arrayWordState = [];
+var arrayWord = words[0].split("");//[Math.floor(Math.random() * words.length)].split("");
+//var userGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//var arrayWordState = [];
 
 //selects random word and assigns "_"
-for (i = 0; i < arrayWord.length; i++) {
-    arrayWordState.push("_");
+document.onkeyup = function randomWord() {
+    arrayWordState = [];
+    arrayWord = words[Math.floor(Math.random() * words.length)];
     console.log(arrayWord);
-}
-
-//listen for user input
-document.onkeyup = function(event) {
-    var keyPressed = event.key.toLowerCase();
-    console.log(keyPressed);
-
     for (i = 0; i < arrayWord.length; i++) {
-        if (arrayWord[i] == keyPressed) {
-            arrayWordState[i] = keyPressed;
-            console.log(arrayWordState);
-        }
+    arrayWordState.push("_");
+    document.getElementById("word-blanks").innerHTML = arrayWordState;
+    console.log(arrayWordState);
     }
 }
+//listens for user input, assigns input to corresponding "_"
+document.onkeypress = function userSelect(event) {
+    arrayWordState = [];
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+     //keyPressed = userGuess;
+    for (i = 0; i < arrayWord.length; i++) {
+        if (arrayWord[i] == userGuess) {
+            arrayWordState[i] = userGuess;
+            console.log(arrayWordState);
+           } 
+    }
+}
+
+
  
 
 
